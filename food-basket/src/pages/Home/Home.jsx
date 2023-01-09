5ec3a6a7a76efe843321419d2912216dfb1c5389
Mayurwagh98 <mswagh1998@gmail.com>
2023-01-09 08:04:40 +0530

    Added: Our Nutrition Banner

--- food-basket/src/pages/Home/Home.jsx
+++ food-basket/src/pages/Home/Home.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Navbar } from "../../components/Navbar/Navbar";
 import rooted from "../../images/rooted2.png";
-import "./Home.css"
-import {AdvancedCarousel} from "../../components/Slider"
+import "./Home.css";
+import { AdvancedCarousel } from "../../components/Slider";
 
 const Home = () => {
   return (
@@ -11,7 +11,7 @@ const Home = () => {
         <Navbar />
       </div>
       <div className="rooted_div">
-        <img src={rooted} alt=""  />
+        <img src={rooted} alt="" />
       </div>
       <div className="featured_products_heading">
         <h3>BEST-SELLING WELLNESS ESSENTIALS</h3>
@@ -19,6 +19,18 @@ const Home = () => {
       </div>
       <div>
         <AdvancedCarousel />
+      </div>
+      <div className="philosophy_div">
+        <div className="philosophy_heading">
+          <h2>Our Nutrition Philosophy</h2>
+        </div>
+        <div className="philosophy_content">
+          <h3>
+            We believe in a whole-food, plant-rich diet. Our proprietary
+            science-backed nutrition philosophy delivers life-transforming
+            results.
+          </h3>
+        </div>
       </div>
     </>
   );
