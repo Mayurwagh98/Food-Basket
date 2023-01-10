391643ce6a9c0dd74a5fc088c4930056bac3bca6
Mayurwagh98 <mswagh1998@gmail.com>
2023-01-10 11:25:18 +0530

    Completed: AllProducts Page

--- food-basket/src/pages/AllProducts/Allproducts.jsx
+++ food-basket/src/pages/AllProducts/Allproducts.jsx
@@ -1,21 +1,28 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Navbar } from "../../components/Navbar/Navbar";
-import delicious_product from "../../images/delicious_product.png";
+import delicious_banner from "../../images/delicious_banner.png";
 import "./Allproducts.css";
 import { Button } from "antd";
 import { Footer } from "../../components/Footer/Footer";
+import ScrollToTop from "react-scroll-to-top";
+import brc from "../../images/brc.png";
+import gluten_free from "../../images/gluten_free.png";
+import glycemic from "../../images/glycemic.png";
+import organic_product from "../../images/organic_product.png";
+import vegan from "../../images/vegan.png";
+import { useNavigate } from "react-router-dom";
 
 const Allproducts = () => {
   let [products, setProducts] = useState([]);
-
+  let naviagate = useNavigate();
   let getProducts = () => {
     axios
       .get("http://localhost:8080/allProducts")
       .then((res) => {
         products = res.data;
         setProducts(products);
-        console.log(products);
+        // console.log(products);
       })
       .catch((e) => {
         console.log(e);
@@ -26,13 +33,16 @@ const Allproducts = () => {
     getProducts();
   }, []);
 
+  let handleNavigate = () => {
+    naviagate("/organic_coconut_sweetners");
+  };
   return (
     <>
       <Navbar />
-
+      <ScrollToTop smooth />
       <div className="main_prduct_div">
         <div className="product_banner">
-          <img src={delicious_product} alt="productImage" />
+          <img src={delicious_banner} alt="productImage" />
         </div>
         <div className="product_div">
           {products.map((item, index) => {
@@ -41,10 +51,19 @@ const Allproducts = () => {
                 <img src={item.image} alt="" />
                 <h1>{item.title}</h1>
                 <p>{item.description}</p>
-                <Button type="primary">Shop Now</Button>
+                <Button type="primary" onClick={handleNavigate}>
+                  Shop Now
+                </Button>
               </div>
             );
           })}
+        </div>
+        <div className="logos">
+          <img src={brc} alt="" />
+          <img src={gluten_free} alt="" />
+          <img src={glycemic} alt="" />
+          <img src={organic_product} alt="" />
+          <img src={vegan} alt="" />
         </div>
         <Footer />
       </div>
