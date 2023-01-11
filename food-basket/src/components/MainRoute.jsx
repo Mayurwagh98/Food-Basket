4e8110ba92e246fcd04cbc088939be8ac82a8544
Mayurwagh98 <mswagh1998@gmail.com>
2023-01-11 07:14:53 +0530

    Added: Remaning Routes

--- food-basket/src/components/MainRoute.jsx
+++ food-basket/src/components/MainRoute.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import { About } from "../pages/About/About";
+import { Allproducts } from "../pages/AllProducts/Allproducts";
 import { Home } from "../pages/Home/Home";
+import {Organic_coconut_sweetners} from "../pages/Organic_coconut_sweetners/Organic_coconut_sweetners"
+import {ViewAll} from "../pages/ViewAll/ViewAll"
+import { OrganicDetails } from "../pages/OrganicDetails/OrganicDetails";
 
 const MainRoute = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/allProducts" element={<Allproducts />} />
+        <Route path="/organic_coconut_sweetners" element={<Organic_coconut_sweetners />} />
+        <Route path="/recipes" element={<ViewAll />} />
+        <Route path="/organic_details" element={<OrganicDetails />} />
       </Routes>
     </div>
   );
