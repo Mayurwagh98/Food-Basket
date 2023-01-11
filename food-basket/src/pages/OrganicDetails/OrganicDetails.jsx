8d76603b990da69aedc69e36caa51130592a3108
Mayurwagh98 <mswagh1998@gmail.com>
2023-01-11 11:00:10 +0530

    Working On: Details Page

--- food-basket/src/pages/OrganicDetails/OrganicDetails.jsx
+++ food-basket/src/pages/OrganicDetails/OrganicDetails.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Navbar } from "../../components/Navbar/Navbar";
+import "./OrganicDetails.css";
 
 const OrganicDetails = () => {
-  return (
-    <div>
-      <h1>Organic details</h1>
-    </div>
-  )
-}
+  let { id } = useParams();
+  let [data, setData] = useState({});
 
-export {OrganicDetails}
+  let getItem = () => {
+    axios
+      .get(`http://localhost:8080/organic_sweetners/${id}`)
+      .then((res) => {
+        setData(res.data);
+        console.log(res.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  useEffect(() => {
+    getItem();
+  }, []);
+
+  return (
+    <div className="main_details_div">
+      <Navbar />
+      <div className="child_details_div">
+        <div>
+          <h2>{data.title}</h2>
+          <h3>{data.details}</h3>
+        </div>
+        <div>
+          <img src={data.image} alt="item image" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export { OrganicDetails };
