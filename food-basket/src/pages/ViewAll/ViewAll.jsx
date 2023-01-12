100aade589906fb2d929319511e3621d5f1ed0cb
Mayurwagh98 <mswagh1998@gmail.com>
2023-01-12 09:44:28 +0530

    Added: Pagination and css

--- food-basket/src/pages/ViewAll/ViewAll.jsx
+++ food-basket/src/pages/ViewAll/ViewAll.jsx
@@ -3,10 +3,11 @@ import React, { useEffect, useState } from "react";
 import { Navbar } from "../../components/Navbar/Navbar";
 import explore_recipes from "../../images/explore_recipes.png";
 import "./ViewAll.css";
-import { Button } from "antd";
+import ReactPaginate from "react-paginate";
 
 const ViewAll = () => {
   let [recipes, setRecieps] = useState([]);
+  const [currPage, setCurrPage] = useState(0);
 
   let getRecipes = () => {
     axios
@@ -23,6 +24,14 @@ const ViewAll = () => {
     getRecipes();
   }, []);
 
+  // ------------------- Pagination -------------------
+  const perpage = 8;
+  let handleFetch = ({ selected: selectedPage }) => {
+    setCurrPage(selectedPage);
+  };
+  const pageCount = Math.ceil(recipes.length / perpage);
+  const offset = currPage * perpage; //offset = 0, 10, 20......
+
   return (
     <>
       <Navbar />
@@ -30,21 +39,33 @@ const ViewAll = () => {
         <div className="recipes_img">
           <img src={explore_recipes} alt="" />
         </div>
-        <h1>Recepies</h1>
+        <h1 className="recipe_heading">All-Recipes</h1>
         <div className="all_recipes">
-          {recipes.map((item, index) => {
+          {recipes.slice(offset, offset + perpage).map((item, index) => {
             return (
               <div key={index}>
                 {/* <div> */}
                 <img src={item.image} alt="" />
                 <h2>{item.title}</h2>
-                <p>{`Product Used - ${item.product_used}`}</p>
+                <h4>{"Product Used - "}</h4>
+                <p>{item.product_used}</p>
                 {/* </div> */}
                 {/* <Button type="primary">Recipe Details</Button> */}
               </div>
             );
           })}
         </div>
+        <ReactPaginate
+          previousLabel={"<- Prev"}
+          nextLabel={"Next ->"}
+          pageCount={pageCount}
+          onPageChange={handleFetch}
+          containerClassName={"pagination"}
+          previousLinkClassName={"pagination__link"}
+          nextLinkClassName={"pagination__link"}
+          disabledClassName={"pagination_link_disabled"}
+          activeClassName={"pagination_link_active"}
+        />
       </div>
     </>
   );
