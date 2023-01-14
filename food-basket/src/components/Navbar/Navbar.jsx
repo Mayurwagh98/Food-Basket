77adc7559a6de335ce0349ac8573d2e5a40c4194
Mayurwagh98 <mswagh1998@gmail.com>
2023-01-14 16:57:28 +0530

    Added: Route to Sauces & Marinades

--- food-basket/src/components/Navbar/Navbar.jsx
+++ food-basket/src/components/Navbar/Navbar.jsx
@@ -2,18 +2,23 @@ import React from "react";
 import { Organics } from "../Organics";
 import { ShopAll } from "../Shopall";
 import "./Navbar.css";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className="main_navbar">
       <div className="heading">
-        <h1>Food-Basket</h1>
+        <Link to="/" style={{ textDecoration: "none", color: "white" }}>
+          <h1>Food-Basket</h1>
+        </Link>
       </div>
       <div className="navbar">
-        <ShopAll />
+        {/* <ShopAll /> */}
         <Organics />
-        <ShopAll />
-        <ShopAll />
+        <Link to="/about" style={{ textDecoration: "none", color: "white" }}>
+          About Us
+        </Link>
+        
       </div>
     </div>
   );
