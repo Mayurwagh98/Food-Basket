0c6e47ccd06fb2bc919d58ef0773032e08b13298
Mayurwagh98 <mswagh1998@gmail.com>
2023-01-14 07:39:18 +0530

    Added: Sauces Route

--- food-basket/src/components/MainRoute.jsx
+++ food-basket/src/components/MainRoute.jsx
@@ -3,9 +3,11 @@ import { Routes, Route } from "react-router-dom";
 import { About } from "../pages/About/About";
 import { Allproducts } from "../pages/AllProducts/Allproducts";
 import { Home } from "../pages/Home/Home";
-import {Organic_coconut_sweetners} from "../pages/Organic_coconut_sweetners/Organic_coconut_sweetners"
-import {ViewAll} from "../pages/ViewAll/ViewAll"
+import { Organic_coconut_sweetners } from "../pages/Organic_coconut_sweetners/Organic_coconut_sweetners";
+import { ViewAll } from "../pages/ViewAll/ViewAll";
 import { OrganicDetails } from "../pages/OrganicDetails/OrganicDetails";
+import { Recipes } from "../pages/Recipes/Recipes";
+import { Sauces } from "../pages/Sauces/Sauces";
 
 const MainRoute = () => {
   return (
@@ -14,9 +16,17 @@ const MainRoute = () => {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/allProducts" element={<Allproducts />} />
-        <Route path="/organic_coconut_sweetners" element={<Organic_coconut_sweetners />} />
+        <Route
+          path="/organic_coconut_sweetners"
+          element={<Organic_coconut_sweetners />}
+        />
         <Route path="/viewAll" element={<ViewAll />} />
-        <Route path="/organic_coconut_sweetners/:id/organic_details" element={<OrganicDetails />} />
+        <Route
+          path="/organic_coconut_sweetners/:id/organic_details"
+          element={<OrganicDetails />}
+        />
+        <Route path="/viewAll/:id" element={<Recipes />} />
+        <Route path="/sauces" element={<Sauces />} />
       </Routes>
     </div>
   );
