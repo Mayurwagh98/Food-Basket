525e1155b743500a9efa210bf72f0b85382fc95c
Mayurwagh98 <mswagh1998@gmail.com>
2023-01-14 17:51:03 +0530

    Added: Authentication

--- food-basket/src/components/Navbar/Navbar.jsx
+++ food-basket/src/components/Navbar/Navbar.jsx
@@ -3,8 +3,12 @@ import { Organics } from "../Organics";
 import { ShopAll } from "../Shopall";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Button } from "antd";
 
 const Navbar = () => {
+  const { loginWithRedirect, logout, user, isAuthenticated } = useAuth0();
+
   return (
     <div className="main_navbar">
       <div className="heading">
@@ -18,7 +22,22 @@ const Navbar = () => {
         <Link to="/about" style={{ textDecoration: "none", color: "white" }}>
           About Us
         </Link>
-        
+        {/* {isAuthenticated && <p>{user.name}</p>} */}
+        {isAuthenticated ? (
+          <Link
+            style={{ textDecoration: "none", color: "white", margin: "10px" }}
+            onClick={() => logout()}
+          >
+            Logout
+          </Link>
+        ) : (
+          <Link
+            style={{ textDecoration: "none", color: "white", margin: "10px" }}
+            onClick={() => loginWithRedirect()}
+          >
+            Login/Signup
+          </Link>
+        )}
       </div>
     </div>
   );
