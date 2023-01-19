60500576b045fc6f700eae2f98714c5e25af4c95
Mayurwagh98 <mswagh1998@gmail.com>
2023-01-19 17:15:17 +0530

    Wrapped: App in Auth0Provider

--- food-basket/src/main.jsx
+++ food-basket/src/main.jsx
@@ -3,9 +3,16 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Auth0Provider
+    domain="dev-7qg8ax7i1o0mufu4.us.auth0.com"
+    clientId="dUP7oAkWNODceKSIrJfn4wbpzjZsjgbe"
+    redirectUri={window.location.origin}
+  >
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Auth0Provider>
 );
