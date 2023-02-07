a12f89898507cc4e37811a47b0a6445fd88da110
Mayurwagh98 <mswagh1998@gmail.com>
2023-02-08 00:11:09 +0530

    Filtering: Data based on Category

--- food-basket/src/pages/ShopAll/ShopAll.jsx
+++ food-basket/src/pages/ShopAll/ShopAll.jsx
@@ -5,6 +5,7 @@ import "./ShopAll.css";
 
 const ShopAll = () => {
   let [shopData, setShopData] = useState([]);
+  let [isChecked, setIsChecked] = useState(false);
 
   let getShopData = () => {
     axios
@@ -22,6 +23,34 @@ const ShopAll = () => {
     getShopData();
   }, []);
 
+  let handleChange = (event) => {
+    // console.log(event.target.value)
+    if (event.target.value == "") {
+      axios
+        .get(`http://localhost:8080/sakara`)
+        .then((res) => {
+          // console.log(res.data);
+          setShopData(res.data);
+          isChecked ? setIsChecked(true) : setIsChecked(false);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    } else {
+      axios
+        .get(`http://localhost:8080/sakara?category=${event.target.value}`)
+        .then((res) => {
+          // console.log(res.data);
+          setShopData(res.data);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    }
+  };
+
+ 
+
   return (
     <>
       <Navbar />
@@ -31,18 +60,28 @@ const ShopAll = () => {
       <div className="filter_shop_div">
         <h2>Refine By Category</h2>
         <label>
-          <input type="checkbox" />
+          <input
+            type="checkbox"
+            onChange={handleChange}
+            value=""
+            
+          />
           All Products
         </label>
         <br />
         <label htmlFor="">
-          <input type="checkbox" />
+          <input
+            type="checkbox"
+            onChange={handleChange}
+            value="Best Seller"
+            
+          />
           Best Sellers
         </label>
         <br />
         <label htmlFor="">
-          <input type="checkbox" />
-          New Sellers
+          <input type="checkbox" onChange={handleChange} value="Featured" />
+          Featured
         </label>
       </div>
       <div className="main_shop_div">
